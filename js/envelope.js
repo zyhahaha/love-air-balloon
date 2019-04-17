c763377edfae7a9869cc14c2dbd0ce4427f7fab2
zhaoyang <18355403288@163.com>
2019-04-17 16:27:22 +0800

    update

--- js/envelope.js
+++ js/envelope.js
@@ -1,15 +1,12 @@
 import animate from './common/animateplus.js';
-var envelopeEl = document.getElementsByClassName('envelope-page')[0];
+let envelopeEl = document.getElementsByClassName('envelope-page')[0];
 
-const play = async () => {
-  const options = await animate({
+const envelopePlay = async () => {
+  await animate({
     elements: envelopeEl,
     duration: 3000,
     transform: ['translateY(-100vh)', 0]
   });
 };
-window.onload = function() {
-  setTimeout(() => {
-    play();
-  }, 2000);
-};
+
+export default envelopePlay;
--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 import './common/device-init.js';
-// import './air-balloon.js';
-// import './envelope.js';
+import './air-balloon.js';
+import envelopePlay from './envelope.js';
+
+// var myAuto = document.getElementById('myaudio');
+// myAuto.play();
+
+setTimeout(() => {
+  envelopePlay();
+}, 2000);
