9b6f68f0c3c582b48701e85471266a5076cd6f99
zhaoyang <18355403288@163.com>
2019-04-18 14:18:54 +0800

    update

--- js/paper.js
+++ js/paper.js
@@ -1,6 +1,14 @@
 import animate from './common/animateplus.js';
 let paperEl = document.getElementsByClassName('paper-page')[0];
 
+let str = `<h3>周年快乐</h3>
+            <p>
+              小胖孩，周年快乐，我们已经在一起一年啦 ~~~ <br />
+              老公是不是特别好，嘿嘿
+              每年都会给你准备周年小惊喜哦
+            </p>`;
+paperEl.innerHTML = str;
+
 const paperPlay = async () => {
   await animate({
     elements: paperEl,
