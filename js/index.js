8bf06cbe59e829bf3ad52d40c178cc10b5ae8cf3
zhaoyang <18355403288@163.com>
2019-04-18 10:56:42 +0800

    update

--- js/index.js
+++ js/index.js
@@ -3,8 +3,5 @@ import './air-balloon.js';
 import './envelope.js';
 import paperPlay from './paper.js';
 
-// var myAuto = document.getElementById('myaudio');
-// myAuto.play();
-
 let heartEl = document.getElementsByClassName('heart')[0];
 heartEl.addEventListener('click', paperPlay);
